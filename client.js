11e23d46d3a9b0045bc979eea22a47de26cdd1b8
FIZZO PANNOSCH <fizzo999@gmail.com>
2021-05-10 18:17:09 -0700

    adds readme file

--- client.js
+++ client.js
@@ -3,12 +3,12 @@
 const io = require('socket.io-client');
 
 // const server = 'http://localhost:3000';
-const server = 'http://nodeservertest-env.eba-tje4rh3r.us-west-2.elasticbeanstalk.com';
+const server = 'http://fizzosocketioservertest-env.eba-6g4rpcpc.us-west-2.elasticbeanstalk.com/';
 
 const socket = io.connect(server);
 
 socket.on('welcome', payload => console.log(payload));
 
-socket.emit('hello', 'John');
+socket.emit('hello', 'FIZZO THERE YOU ARE');
 
 
